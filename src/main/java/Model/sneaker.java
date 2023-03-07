bafb4ef31943b7516970146632c712e6c22675df
Sadrach <sadrach@Jesuss-MacBook-Air.local>
2023-03-07 12:39:20 -0800

    Completed Model

--- src/main/java/Model/sneaker.java
+++ src/main/java/Model/sneaker.java
@@ -1,4 +1,17 @@
 package Model;
 
 public class sneaker {
+    public String color;
+    public String name;
+    public String brand;
+    public double price;
+
+    public sneaker(String color, String name, String brand, double price) {
+        this.color = color;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+    }
+
+
 }
