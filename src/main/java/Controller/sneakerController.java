c60fe85c6756b5b703c18c7f7d41dc85c98b5f34
unknown <tajdaley@yahoo.comgit config --global user.name TajahDaley>
2023-03-07 14:26:48 -0600

    Updating the controller

--- src/main/java/Controller/sneakerController.java
+++ src/main/java/Controller/sneakerController.java
@@ -1,4 +1,29 @@
 package Controller;
 
 public class sneakerController {
+    SneakerService sneakerService;
+    public sneakerController(){
+        sneakerService = new SneakerService();
+    }
+
+    public Javalin StartAPI(){
+        Javalin app = Javalin.create();
+        app.get("/sneakers", this::getAllSneakers);
+        app.get("/sneakers/{brand_name}", this::getSneakersByBrand);
+        app.get("/sneakers/color/{color}", this::getSneakersByColor);
+        return app;
+    }
+
+    private void getAllSneakers(context ctx) throws JsonProcessingException {
+        ctx.json(sneakerService.getAllSneakers());
+    }
+
+    private void getSneakersByBrand(context ctx) throws JsonProcessingException {
+        ctx.json(sneakerService.getSneakersByBrand());
+    }
+
+    private void getSneakersByColor(context ctx) throws JsonProcessingException {
+        ctx.json(sneakerService.getSneakersByColor());
+    }
+
 }
