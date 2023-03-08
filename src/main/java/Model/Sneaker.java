d89fa68a1d212f2e6341669aba015b1e6cd747a5
unknown <tajdaley@yahoo.comgit config --global user.name TajahDaley>
2023-03-08 14:12:07 -0600

    update model

--- src/main/java/Model/Sneaker.java
+++ src/main/java/Model/Sneaker.java
@@ -1,15 +1,15 @@
 package Model;
 
 public class Sneaker {
-    public String color;
     public String name;
     public String brand;
+    public String color;
     public String price;
 
     public Sneaker(String color, String name, String brand, String price) {
-        this.color = color;
         this.name = name;
         this.brand = brand;
+        this.color = color;
         this.price = price;
     }
 
