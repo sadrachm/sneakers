53c3c009f32da538a60a7cf847108a5ba741e1c6
unknown <tajdaley@yahoo.comgit config --global user.name TajahDaley>
2023-03-10 17:39:29 -0600

    updating controller with a patch request

--- src/main/java/Controller/SneakerController.java
+++ src/main/java/Controller/SneakerController.java
@@ -20,6 +20,7 @@ public class SneakerController {
         app.get("/sneakers/{brand_name}", this::getSneakersByBrand);
         app.get("/sneakers/color/{color}", this::getSneakersByColor);
         app.post("/sneakers", this::addNewSneaker);
+        app.patch("sneakers", this::updateSneaker);
         return app;
     }
 
@@ -44,6 +45,13 @@ public class SneakerController {
         } else {
             ctx.status(200);
         }
+    }
+
+    private void updateAmount(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Sneaker sneaker = mapper.readValue(ctx.body(), Sneaker.class);
+
+
     }
 
 }
